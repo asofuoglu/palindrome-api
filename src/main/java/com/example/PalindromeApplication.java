c483a696c3c9be28cf95af32b72203be7808442a
asofuoglu <a.sofuoglu1@gmail.com>
2024-09-24 20:19:44 +0300

    added

--- src/main/java/com/example/PalindromeApplication.java
+++ src/main/java/com/example/PalindromeApplication.java
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PalindromeApplication {
   public static void main(String[] args) {
     SpringApplication.run(PalindromeApplication.class, args);
+
+
   }
 }
